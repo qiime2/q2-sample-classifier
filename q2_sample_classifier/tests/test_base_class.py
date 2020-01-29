6809a0ed655828f203748a3047fd91b6b66c31de
Anthony <oddant1@hotmail.com>
2020-01-29 10:24:16 -0700

    SQUASH: Linter

--- q2_sample_classifier/tests/test_base_class.py
+++ q2_sample_classifier/tests/test_base_class.py
@@ -1,3 +1,10 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2017-2020, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
 import tempfile
 import pkg_resources
 
