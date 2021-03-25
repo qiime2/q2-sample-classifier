f44562611dddf9acec11aa04174349c2d72cbdf9
Matthew Dillon <matthewrdillon@gmail.com>
2021-03-25 15:50:52 -0700

    Revert "squash: index name"
    This reverts commit 475c2b14f24c3d804fd14b7f215f325c9e83c00b.

--- q2_sample_classifier/tests/test_estimators.py
+++ q2_sample_classifier/tests/test_estimators.py
@@ -289,8 +289,7 @@ class EstimatorsTests(SampleClassifierTestPluginBase):
             name='prediction')
         exp_importances = pd.DataFrame(
             [0.595111111111111, 0.23155555555555551, 0.17333333333333334],
-            index=pd.Index(['o3', 'o1', 'o2'], name='feature'),
-            columns=['importance'])
+            index=pd.Index(['o3', 'o1', 'o2']), columns=['importance'])
         exp_probabilities = pd.DataFrame(
             [[0.5, 0.5], [0., 1.], [0., 1.], [0.5, 0.5], [0.5, 0.5],
              [0.5, 0.5], [0.5, 0.5], [0., 1.], [1., 0.], [1., 0.]],
