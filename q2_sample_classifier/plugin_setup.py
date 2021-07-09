43fdb1fc0ddec2efbcb0559f25f6dd399e0328b6
adamovanja <57316423+adamovanja@users.noreply.github.com>
2021-07-09 11:10:01 +0200

    FIX: added spaces

--- q2_sample_classifier/plugin_setup.py
+++ q2_sample_classifier/plugin_setup.py
@@ -239,9 +239,9 @@ plugin.pipelines.register_function(
         'probabilities': input_descriptions['probabilities'],
         'heatmap': 'A heatmap of the top 50 most important features from the '
                    'table.',
-        'training_targets': 'Series containing true target values of'
+        'training_targets': 'Series containing true target values of '
         'train samples',
-        'test_targets': 'Series containing true target values'
+        'test_targets': 'Series containing true target values '
         'of test samples'},
     name='Train and test a cross-validated supervised learning classifier.',
     description=description.format(
@@ -508,9 +508,9 @@ plugin.methods.register_function(
     output_descriptions={
         'training_table': 'Feature table containing training samples',
         'test_table': 'Feature table containing test samples',
-        'training_targets': 'Series containing true target values of'
+        'training_targets': 'Series containing true target values of '
         'train samples',
-        'test_targets': 'Series containing true target values of'
+        'test_targets': 'Series containing true target values of '
         'test samples'},
     name='Split a feature table into training and testing sets.',
     description=(
